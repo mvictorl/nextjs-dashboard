4bde85d1184b683189a3bbd5cef11dd265b4897f
MVictorL <mvictorl@gmail.com>
2024-08-08 11:17:00 +0300

    SingOut to root page

--- auth.ts
+++ auth.ts
@@ -22,18 +22,16 @@ export const { auth, signIn, signOut } = NextAuth({
     Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
-          .object({
-            email: z.string().email(),
-            password: z.string().min(6),
-          })
+          .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials)
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data
+
           const user = await getUser(email)
           if (!user) return null
-          const passwordsMatch = await bcrypt.compare(password, user.password)
 
+          const passwordsMatch = await bcrypt.compare(password, user.password)
           if (passwordsMatch) return user
         }
 
